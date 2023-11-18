5e46c7dd0a25e972e03da0a207cb68366f452877
WENDEL4891 <42788239+WENDEL4891@users.noreply.github.com>
2023-11-18 00:20:58 -0300

    Update main.py
    Incluída rota para gerar dados aleatórios de usuários

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get("/fake-users")
+async def fake_users():
+    users = []
+    for _ in range(100):
+        user = {
+            "nome": fake.name(),
+            "data_nascimento": fake.date_of_birth(minimum_age=18, maximum_age=90).isoformat(),
+            "naturalidade": fake.city(),
+            "cpf": fake.cpf(),
+            "foto": f"https://via.placeholder.com/150?text={random.randint(1, 1000)}"
+        }
+        users.append(user)
+
+    return users
